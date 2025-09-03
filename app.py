0193d555320461d2818379a595089943eba58dc6
muktimukashi <103751405+muktimukashi@users.noreply.github.com>
2025-09-03 13:29:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ supabase = create_client(supabase_url, supabase_key)
 
 # === KONFIGURASI STREAMLIT ===
 st.set_page_config(page_title="Peminjaman iPhone 13", page_icon="📱")
-st.title("📱 Sistem Peminjaman iPhone 13 (Supabase)")
+st.title("📱 Sistem Peminjaman iPhone 13")
 
 # === FUNGSI BANTUAN ===
 def get_data():
